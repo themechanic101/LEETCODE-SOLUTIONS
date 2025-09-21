a64fc40980cf176f492abfcd0a678a1fd14f63ac
themechanic101 <168337418+themechanic101@users.noreply.github.com>
2025-09-21 15:15:10 +0530

    Time: 4 ms (39.41%) | Memory: 9.5 MB (98.94%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        for(int i =0; i<max(s.size(),t.size());i++){
+            if(s[i]!=t[i]) return false;
+        }
+
+        return true;
+        
+    }
+};
