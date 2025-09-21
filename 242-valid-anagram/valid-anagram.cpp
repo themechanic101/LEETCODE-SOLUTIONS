9efb8867c1d9bfbee4242cb460e3c69f598cbe76
themechanic101 <168337418+themechanic101@users.noreply.github.com>
2025-09-21 16:23:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.24%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,14 +2,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());  //O(nlogn ) is the tc 
+        // sort(t.begin(),t.end());
 
-        for(int i =0; i<max(s.size(),t.size());i++){
-            if(s[i]!=t[i]) return false;
+        // for(int i =0; i<max(s.size(),t.size());i++){
+        //     if(s[i]!=t[i]) return false;
+        // }
+
+        // return true;
+
+        if(s.size()!=t.size()) return false;
+
+        unordered_map<char,int>map;
+
+        for(int i=0;i<s.size();i++){
+            map[s[i]]++;
+        }
+        for (int i=0 ;i<t.size();i++){
+            if(map.find(t[i])!= map.end()){
+                map[t[i]]--;
+            }else{
+                return false;
+            }  
         }
 
-        return true;
+        for (auto items : map){
+            if(items.second!=0)return false; 
+        }
+          return true;
         
     }
 };
