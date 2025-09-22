e32298dfbdaa9ca4e3a5046b36f89d6e172a1698
themechanic101 <168337418+themechanic101@users.noreply.github.com>
2025-09-23 03:44:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (60.11%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+
+public:
+ void findcombination(int ind,int target, vector<int>&arr , vector<vector<int>>&ans,vector<int>&ds){
+    if(target==0){
+        ans.push_back(ds);
+        return;
+    }
+// arr=candidates array
+// 
+    for(int i=ind;i<arr.size();i++){
+        // if same element in iteration then skip or not take the same element
+        if(i>ind && arr[i]==arr[i-1])continue;
+        if(arr[i]>target)break;
+        ds.push_back(arr[i]);
+        findcombination(i+1,target-arr[i],arr,ans,ds);
+        ds.pop_back();
+        
+    }
+ }
+
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+        // using combinational sum approach will give tle
+        sort(candidates.begin(),candidates.end());
+        vector< vector<int>>ans;
+        vector<int>ds;
+        findcombination(0,target,candidates,ans,ds);
+        return ans;
+        
+    }
+};
