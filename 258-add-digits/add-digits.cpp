1960ddeb6fb37a2cd9430a9a78372350472ba958
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-04 12:09:56 +0530

    Time: 1 ms (6.25%) | Memory: 8.6 MB (44.44%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int addDigits(int num) {
 // all number are epossitive
-        while(num>9){
+        // while(num>9){
+            
+            
             int ans=0 ,digit;
             while(num){
                 digit=num%10;
                 ans+=digit;
                 num=num/10;
             }
-         num=ans;
-        }
-        return num;
+        //  num=ans;
+        // }
+        if(ans%10==ans) return ans;
+        else  return addDigits(ans);
+    
         
     }
 };
