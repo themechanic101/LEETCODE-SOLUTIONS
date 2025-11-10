672807888299d6d77f522461042b3d35446752cd
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-10 22:07:54 +0530

    Time: 27 ms (10.84%) | Memory: 21.2 MB (10.77%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+unordered_set<string>st;
+bool word(string &s,int index,vector<int>&dp){
+      if(index==s.size())return true;
+      if(dp[index]!=-1) return dp[index];
+
+      if(st.find(s)!=st.end())return dp[index]= true;
+
+      for(int i=1;i<=s.size();i++){
+          string curr=s.substr(index,i);
+          if(st.find(curr)!=st.end() && word(s,index+i,dp))return dp[index]= true;
+      }
+      return dp[index]=false;
+}
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int>dp(300,-1);
+        for(string &str: wordDict){
+            st.insert(str);
+        }
+
+      return word(s,0,dp);
+    }
+};
