596dc8f4372e38258de3cd177c7122d8eee40d5c
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-12 14:19:06 +0530

    Time: 3 ms (33.21%) | Memory: 15.5 MB (10.08%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+long long  minimum(vector<vector<int>>&matrix , int row ,int col ,  vector<vector<long long>>&dp){
+    // if(row<0 || col<0)return 0;
+    if(col<0 || col>matrix.size()-1)return INT_MAX;
+    if(row==0) return matrix[0][col];
+    if(dp[row][col]!=INT_MIN) return dp[row][col];
+
+    long long up=matrix[row][col]+minimum(matrix,row-1,col,dp);
+    long long  d_left=matrix[row][col]+minimum(matrix,row-1,col-1,dp);
+    long long d_right=matrix[row][col]+minimum(matrix,row-1,col+1,dp);
+
+    return dp[row][col]=min({up,d_left,d_right});
+}
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<long long>>dp(n,vector<long long>(n,INT_MIN));
+        long long ans =INT_MAX;
+        for(int i=0;i<n;i++){
+            ans=min(ans,minimum(matrix,n-1,i,dp));
+        }
+
+        return ans;
+    }
+};
