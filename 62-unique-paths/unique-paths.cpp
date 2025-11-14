6432504515dd5d98bc0937f1c59192e5b05fb85d
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-14 11:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (44.59%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
-    int path( vector<vector<int>>&dp,int row,int col,int m,int n ){
-        if(row==0 && col==0)return 1;//base condition
-        if(row<0 || col<0) return 0; // out of bound
-        if(dp[row][col]!=-1)return dp[row][col]; // remove overlapping
-        return dp[row][col]=path(dp,row-1,col,m,n)+path(dp,row,col-1,m,n);
-    }
+   
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        vector<vector<int>>dp(m,vector<int>(n,1));
         if(m<0 || n<0)return 0;
         dp[0][0]=1;
         for(int i=1;i<m;i++){
@@ -16,6 +11,6 @@ public:
             }
         }
 
-        return abs(dp[m-1][n-1]);
+        return dp[m-1][n-1];
     }
 };
