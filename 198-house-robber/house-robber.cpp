6e41cd23eca21d0b6c1cff0c860829dac0ca6873
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-14 10:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.78%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,32 @@
 class Solution {
-public:
-int money(vector<int>&nums,int index,vector<int>&dp){
-    if(index<0)return 0;
-    if(index==0) return nums[0];
+// public:
+// int money(vector<int>&nums,int index,vector<int>&dp){
+//     if(index<0)return 0;
+//     if(index==0) return nums[0];
 
-    if(dp[index]!=-1) return dp[index];
+//     if(dp[index]!=-1) return dp[index];
 
-    int yes=nums[index]+money(nums,index-2,dp);
+//     int yes=nums[index]+money(nums,index-2,dp);
 
-    int no=money(nums,index-1,dp);
+//     int no=money(nums,index-1,dp);
 
-    return dp[index]=max(yes,no);
+//     return dp[index]=max(yes,no);
 
 
-}
+// }
 public:
     int rob(vector<int>& nums) {
       int n =nums.size();
-      vector<int>dp(n+1,-1);
-      return money(nums,n-1,dp);
+      vector<int>dp(n);
+      if(n==0)return 0;
+      if(n==1)return nums[0];
+
+      dp[0]=nums[0];
+      dp[1]=max(nums[0],nums[1]);
+
+      for(int i=2;i<n;i++){
+        dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+      }
+      return dp[n-1];
     }
 };
