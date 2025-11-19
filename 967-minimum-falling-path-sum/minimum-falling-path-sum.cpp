a41aeaab39519d5489d9df7f5b3cd152ef67e550
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-19 22:55:41 +0530

    Time: 4 ms (20.76%) | Memory: 15.6 MB (8.84%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
 long long  minimum(vector<vector<int>>&matrix , int row ,int col ,  vector<vector<long long>>&dp){
-    // if(row<0 || col<0)return 0;
     if(col<0 || col>matrix.size()-1)return INT_MAX;
     if(row==0) return matrix[0][col];
     if(dp[row][col]!=100000) return dp[row][col];
@@ -14,7 +13,7 @@ long long  minimum(vector<vector<int>>&matrix , int row ,int col ,  vector<vecto
 }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<long long>>dp(n,vector<long long>(n,100000));
+        vector<vector<long long>>dp(n,vector<long long>(n,1e5));
         long long ans =INT_MAX;
         for(int i=0;i<n;i++){
             ans=min(ans,minimum(matrix,n-1,i,dp));
