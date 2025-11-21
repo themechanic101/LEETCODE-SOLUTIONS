b3d28caebed8108eb7577315298d941fc4f0861d
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-11-21 22:36:58 +0530

    Time: 3 ms (33.94%) | Memory: 15.7 MB (8.92%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,7 +4,7 @@ long long  minimum(vector<vector<int>>&matrix , int row ,int col ,  vector<vecto
     if(col<0 || col>matrix.size()-1)return INT_MAX;
     if(row==0) return matrix[0][col];
     if(dp[row][col]!=100000) return dp[row][col];
-
+// kjhjkh
     long long up=matrix[row][col]+minimum(matrix,row-1,col,dp);
     long long  d_left=matrix[row][col]+minimum(matrix,row-1,col-1,dp);
     long long d_right=matrix[row][col]+minimum(matrix,row-1,col+1,dp);
