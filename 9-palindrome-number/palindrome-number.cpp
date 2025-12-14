e4a7e3b2ef2babf04cdeb7a2379b51a3b5c6cb49
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-12-15 03:51:49 +0530

    Time: 3 ms (46.60%) | Memory: 10.8 MB (22.05%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        string s=to_string(x);
+
+        int l=0;
+        int r=s.size()-1;
+
+        while(l<r){
+            if(s[l]!=s[r])return false;
+
+            l++;
+            r--;
+        }
+  return true;
+       
+    }
+};
