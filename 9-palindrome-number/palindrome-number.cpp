19168bd87bd8e93a458b3d5331ce1de0136e1e51
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-12-15 03:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (91.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0)return false;
-        string s=to_string(x);
-
-        int l=0;
-        int r=s.size()-1;
-
-        while(l<r){
-            if(s[l]!=s[r])return false;
-
-            l++;
-            r--;
-        }
-  return true;
+        long long rev=0;
+        long long num=x;
+     while (num != 0) {
+    rev = rev * 10 + num % 10; // Add last digit
+    num = num / 10;            // Remove last digit
+}
+  return (rev==x);
        
     }
 };
