564c18108da46ac76a0a75f617b30882ebc3328b
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2025-12-17 02:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (80.88%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size()/3;
-        unordered_map<int,int>mp;
-        vector<int>ans;
+        int cnt1=0;
+        int cnt2=0;
+        int el1=INT_MIN;
+        int el2=INT_MIN;
 
         for(int i=0;i<nums.size();i++){
-           mp[nums[i]]++;
+            if(cnt1==0  && nums[i]!=el2){
+                el1=nums[i];
+                cnt1=1;
+            }else if(cnt2==0  && nums[i]!=el1){
+                el2=nums[i];
+                cnt2=1;
+            }else if(el1==nums[i])cnt1++;
+            else if(el2==nums[i])cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
+        vector<int>ans;
+        cnt1=0;
+        cnt2=0;
 
-        for(auto &p:mp){
-           if(p.second>n)ans.push_back(p.first);
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==el1)cnt1++;
+            if(nums[i]==el2)cnt2++;
         }
+        int mini=nums.size()/3+1;
+        if(cnt1>=mini)ans.push_back(el1);
+        if(cnt2>=mini)ans.push_back(el2);
 
+   //sort()
         return ans;
     }
 };
