1bafbd317a56cdaf5637f1fa8e63f633f0936ca6
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-03-31 16:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (78.21%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
+    bool binarySearch(vector<int>& nums, int target) {
+        // Get the size of the array
+        int n = nums.size();
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target)return true;
-            }
+        // Initialize low and high pointers for binary search
+        int low = 0, high = n - 1;
+
+        // Binary search loop
+        while (low <= high) {
+            // Calculate the middle index
+            int mid = (low + high) / 2;
+
+            // If the middle element is the target, return true
+            if (nums[mid] == target)
+                return true;
+
+            // If the target is greater, move to the right half
+            else if (target > nums[mid])
+                low = mid + 1;
+
+            // Otherwise, move to the left half
+            else
+                high = mid - 1;
         }
 
+        // Return false if target not found
         return false;
     }
-};
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+
+        int m = matrix[0].size();
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
+
+                return binarySearch(matrix[i], target);
+            }
+           
+        }
+         return false;
+    }
+    };
