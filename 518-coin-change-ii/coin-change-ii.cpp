fff34525265e375027b1c9fbe733f4a22e7636b9
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-08 01:14:11 +0530

    Time: 5 ms (88.66%) | Memory: 10.3 MB (84.58%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-
-    long long solve(vector<int>& coins, int rem, int idx,vector<vector<long long>>&dp) {
-        if (rem == 0)
-            return dp[rem][idx]=1;
-
-            if(dp[rem][idx]!=-1)return dp[rem][idx];
-
-        if (rem < 0 || idx >= coins.size()) {
-            return dp[rem][idx]=0;
-        }
-
-        long long notake = solve(coins, rem, idx + 1,dp);
-        long long take = 0;
-        if (coins[idx] <= rem) {
-             take = solve(coins, rem - coins[idx], idx,dp );
-        }
-
-        return dp[rem][idx]=take + notake;
-    }
     int change(int amount, vector<int>& coins) {
+         vector<long long> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for(int coin : coins){
+            for(int rem = coin; rem <= amount; rem++){
+                dp[rem] += (int)dp[rem - coin];
+            }
+        }
+
+        return dp[amount];
+        // int n = coins.size();
+
+        // vector<vector<long long>> dp(amount + 1, vector<long long>(n + 1, 0));
+
         
-     if(coins.empty())return 0;
-        if (amount == 0)
-            return 1;
-            int n=coins.size();
+        // for (int idx = 0; idx <= n; idx++) {
+        //     dp[0][idx] = 1;
+        // }
 
-       vector<vector<long long>>dp(amount+1,vector<long long>(n+1,-1));
+    
+        // for (int idx = n - 1; idx >= 0; idx--) {
+        //     for (int rem = 1; rem <= amount; rem++) {
 
-        return solve(coins, amount, 0,dp);
+        //         long long notake = dp[rem][idx + 1];
+
+        //         long long take = 0;
+        //         if (coins[idx] <= rem) {
+        //             take = dp[rem - coins[idx]][idx];
+        //         }
+
+        //         dp[rem][idx] = take + notake;
+        //     }
+        // }
+
+        // return (int)dp[amount][0];
     }
 };
