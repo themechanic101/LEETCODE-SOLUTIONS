24f9fe05f991a510ed7a77b36bf7371d9329279b
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-11 23:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.56%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x,0.5);
+        if(x==0)return 0;
+        if(x<2)return 1;
+   int left = 1, right = x / 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = (long long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;   
     }
 };
