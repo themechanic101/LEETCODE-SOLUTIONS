026cd3b60cf4316b48ad9a7307ecacb85e72af05
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-12 22:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (94.73%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        long long ans = 0;
+
+        
+        for (char ch : columnTitle) {
+            ans = ans * 26 + (ch - 'A' + 1);
+        }
+
+        return (int)ans;    
+    }
+};
