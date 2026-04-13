b3cdf479c8cbfddea793d7f43ac65e947cc0bf06
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-14 00:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (41.73%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+         int sum=n*(n+1)/2;
+        int prefix=0;
+        for(int i=1;i<=n;i++){
+            prefix+=i;
+            if(prefix==(sum-prefix+i))
+            return i;
+        }
+        return -1;
+    }
+};
