07db5843f29034c7c60fae3b3e9c23b370ca9eb9
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-21 02:31:22 +0530

    Time: 18 ms (83.42%) | Memory: 17.3 MB (98.80%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        if(n<4)return ans;
+        int n = nums.size();
+        vector<vector<int>> ans;
+        if (n < 4)
+            return ans;
 
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
 
-        for(int i=0;i<n;i++){
-           
-            
-            for(int j=i+1;j<n;j++){
-                           
+        for (int i = 0; i < n; i++) {
 
-               long long target2=1ll*target-1ll*nums[i]-1ll*nums[j];
-               int front=j+1;
-               int back=n-1;
-                     while(front<back){
-                       long long two_sum=nums[front]+nums[back];
+            for (int j = i + 1; j < n; j++) {
 
-                       if(two_sum<target2)front++;
-                       else if (two_sum>target2)back--;
-                       else{
-                        vector<int>quad(4,0);
-                        quad[0]=nums[i];
-                        quad[1]=nums[j];
-                        quad[2]=nums[front];
-                        quad[3]=nums[back];
+                long long target2 =
+                    1ll * target - 1ll * nums[i] - 1ll * nums[j];
+                int front = j + 1;
+                int back = n - 1;
+                while (front < back) {
+                    long long two_sum = nums[front] + nums[back];
+
+                    if (two_sum < target2)
+                        front++;
+                    else if (two_sum > target2)
+                        back--;
+                    else {
+                        vector<int> quad(4, 0);
+                        quad[0] = nums[i];
+                        quad[1] = nums[j];
+                        quad[2] = nums[front];
+                        quad[3] = nums[back];
                         ans.push_back(quad);
-                        
-                       while(front<back && nums[front]==quad[2])front++;
 
-                       while(front<back && nums[back]==quad[3])back--;
-                       }
+                        while (front < back && nums[front] == quad[2])
+                            front++;
 
-                     }
-                     while(j+1<n && nums[j+1]==nums[j])++j;
-
+                        while (front < back && nums[back] == quad[3])
+                            back--;
+                    }
                 }
-                  while(i+1<n && nums[i+1]==nums[i])++i;
+                while (j + 1 < n && nums[j + 1] == nums[j])
+                    ++j;
             }
-    
+            while (i + 1 < n && nums[i + 1] == nums[i])
+                ++i;
+        }
+
         return ans;
     }
 };
