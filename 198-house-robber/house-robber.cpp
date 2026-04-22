f6fdc3fb7bdf6934649d7429d5524dc09e427603
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-23 01:31:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (75.73%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,32 +1,25 @@
 class Solution {
-// public:
-// int money(vector<int>&nums,int index,vector<int>&dp){
-//     if(index<0)return 0;
-//     if(index==0) return nums[0];
 
-//     if(dp[index]!=-1) return dp[index];
-
-//     int yes=nums[index]+money(nums,index-2,dp);
-
-//     int no=money(nums,index-1,dp);
-
-//     return dp[index]=max(yes,no);
-
-
-// }
 public:
+
+
     int rob(vector<int>& nums) {
       int n =nums.size();
-      vector<int>dp(n);
+     
       if(n==0)return 0;
       if(n==1)return nums[0];
 
-      dp[0]=nums[0];
-      dp[1]=max(nums[0],nums[1]);
+       int prev2 = nums[0];                  
+        int prev1 = max(nums[0], nums[1]);    
+        int curr = prev1;
 
-      for(int i=2;i<n;i++){
-        dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
-      }
-      return dp[n-1];
+        for (int i = 2; i < n; i++) {
+            curr = max(prev1, nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+      return curr;
+
+     
     }
 };
