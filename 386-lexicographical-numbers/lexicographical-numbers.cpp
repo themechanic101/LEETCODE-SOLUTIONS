fb30db88a3e516df7315bfb6018795ecf89f7cfc
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-04-27 05:26:04 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (63.57%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    void dfs(int current, int max, vector<int>& result) {
+        if (current > max) return;
+        result.push_back(current);
+        for (int i = 0; i < 10; i++) {
+            int next = current * 10 + i;
+            if (next > max) return;
+            dfs(next, max, result);
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+         vector<int> result;
+        for (int i = 1; i < 10; i++) {
+            dfs(i, n, result);
+        }
+        return result;
+    }
+};
