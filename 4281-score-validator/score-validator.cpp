16df1752820fa2fa88d1a2882a361c750c7df85a
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-05-09 21:41:21 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (49.88%) - LeetSync

--- 4281-score-validator/score-validator.cpp
+++ 4281-score-validator/score-validator.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> scoreValidator(vector<string>& events) {
-        int score=0,cnt=0;
-        vector<int> ans;
-        for(int i=0;i<events.size();i++){
-            
-            if(events[i]>="0" && events[i]<="6" ){
-                score+=stoi(events[i]);
+        auto score=0,counter=0;
+        for(string &x:events){
+            if(isdigit(x[0])){
+                score+=stoi(x);
+            }else if(x=="WD" or x=="NB"){
+                score+=1;
+            }else if(x=="W"){
+                ++counter;
             }
-            else if(events[i]=="W") {
-                cnt++;
-                if(cnt==10) break;
-            }
-            else if(events[i]=="WD") score++;
-            else if(events[i]=="NB") score++;
-
+            if(counter==10)
+                break;
         }
-        ans.push_back(score);
-        ans.push_back(cnt);
-        return ans;
-        
+        return {score,counter};
     }
 };
