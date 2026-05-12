bdc8b6fd5d96c04bbb99fb6cb1529deec8052c59
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-05-12 23:33:22 +0530

    Time: 94 ms (21.85%) | Memory: 110.4 MB (22.82%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     static bool comp(vector<int>&a, vector<int>&b){
-        return (a[1]-a[0])>(b[1]-b[0]);
+        return (a[1]-a[0])<(b[1]-b[0]);
     }
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(),tasks.end(),comp);
 
-        long long ans=0;
-        long long energy=0;
+        int ans=0;
 
-        for(int i=0;i<tasks.size();i++){
-            int actual=tasks[i][0];
-            int minimum=tasks[i][1];
-
-            if(energy<minimum){
-                ans+=(minimum-energy);
-                energy=minimum;
-            }
-            energy-=actual;
+        for(auto task :tasks){
+            ans=max(ans+task[0],task[1]);
         }
-
-        return (int)ans;
+       
+        return ans;
     }
 };
