d2d73f5c712ab434c4feb2aea3b627f6dee4702b
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-05-24 15:42:38 +0530

    Time: 7 ms (72.09%) | Memory: 24 MB (80.30%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+ bool canForm(string& word, vector<int>& counts) {
+        std::vector<int> c(26, 0);
+
+    
+        for (char ch : word) {
+            int x = ch - 'a';
+            c[x]++;
+            if (c[x] > counts[x]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    int countCharacters(vector<string>& words, string chars) {
+        vector<int> counts(26, 0);
+        
+    
+        for (char ch : chars) {
+            counts[ch - 'a']++;
+        }
+
+        int result = 0;
+
+    
+        for (string& word : words) {
+            if (canForm(word, counts)) {
+            
+                result += word.length();
+            }
+        }
+
+        return result;
+    }
+
+   
+};
