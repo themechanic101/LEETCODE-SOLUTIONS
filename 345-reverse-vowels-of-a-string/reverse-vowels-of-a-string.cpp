c325d82dc2fc8a7e47bdd338837b488993c3570c
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-05-27 00:13:45 +0530

    Time: 7 ms (9.00%) | Memory: 10.9 MB (19.87%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-       unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u',
+       set<char> vowels = {'a', 'e', 'i', 'o', 'u',
                              'A', 'E', 'I', 'O', 'U'};
 
         int i=0,j=s.size()-1;
 
-       while (i < j) {
-            
-            while (i < j && vowels.find(s[i]) == vowels.end()) i++;
-            
-            while (i < j && vowels.find(s[j]) == vowels.end()) j--;
+        while(i<j){
+            char l=s[i];
+            char r=s[j];
 
-            if (i < j) {
-                swap(s[i], s[j]);
+            if(vowels.find(l)!=vowels.end() && vowels.find(r)!= vowels.end() && i<j){
+                swap(s[i],s[j]);
                 i++;
                 j--;
             }
+
+            if( vowels.find(r)==vowels.end() && i<j)j--;
+             if( vowels.find(l)==vowels.end() && i<j)i++;
         }
 
         return s;
