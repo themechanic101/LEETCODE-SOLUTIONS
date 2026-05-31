ea9fa217842f39227f5bd9775b1e1b4ff04e8d35
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-05-31 15:52:18 +0530

    Time: 19 ms (96.64%) | Memory: 125.9 MB (5.11%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        int maxasteroid = 0;
-        for(int a : asteroids) {
-            if(a > maxasteroid) maxasteroid = a;
-        }
+        int maxasteroid = *max_element(asteroids.begin(),asteroids.end());
+        
         vector<int> freq(maxasteroid + 1, 0);
         for(int a : asteroids) {
             freq[a]++;
