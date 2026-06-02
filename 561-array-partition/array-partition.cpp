8e2d680172357b852fb4ff2cb8b89a37f0e3d557
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-06-03 00:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (56.86%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int n = nums.size();
+        long long  ans = 0;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i+=2) {
+             ans+=nums[i];
+        }
+
+        return (int)ans;
+    }
+};
