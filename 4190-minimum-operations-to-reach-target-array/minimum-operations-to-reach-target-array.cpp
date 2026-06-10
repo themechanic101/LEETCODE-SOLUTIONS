1ba24cc3f25f14f744617b73d527add65ac6c081
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-06-10 15:27:31 +0530

    Time: 8 ms (95.25%) | Memory: 226.3 MB (98.31%) - LeetSync

--- 4190-minimum-operations-to-reach-target-array/minimum-operations-to-reach-target-array.cpp
+++ 4190-minimum-operations-to-reach-target-array/minimum-operations-to-reach-target-array.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& target) {
-    set<int>a;
-
-    for(int i=0;i<nums.size();i++){
-        if(nums[i] != target[i])a.insert(nums[i]);
-
-    }
-
-    return a.size();
-
-
+       bool seen[100001]{};
+       int ans = 0;
+       for(int i = 0; i < nums.size(); i++){
+        if(nums[i] != target[i] && !seen[nums[i]]){
+            ans++;
+            seen[nums[i]] = true;
+        }
+       }
+       return ans;
     }
 };
