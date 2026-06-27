d2607a81e20c32c6219606bb67fcd44e7c78fbb8
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-06-27 12:17:05 +0530

    Time: 4 ms (57.10%) | Memory: 138.5 MB (12.69%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -10,33 +10,47 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
-                ListNode* slow = head;
+
+    ListNode* middleNode(ListNode* head){
+        ListNode* slow = head;
         ListNode* fast = head;
 
-        while (fast && fast->next) {
+        while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        ListNode* prev = nullptr;
-        while (slow) {
-            ListNode* nxt = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nxt;
+        return slow;
+    }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
+        return prev;
+    }
 
-        int ans = 0;
-        ListNode* first = head;
-        ListNode* second = prev;
-
-        while (second) {
-            ans = max(ans, first->val + second->val);
-            first = first->next;
-            second = second->next;
+    int pairSum(ListNode* head) {
+       vector<int> arr;
+        ListNode *curr=head;
+        while(curr){
+            arr.push_back(curr->val);
+            curr=curr->next;
         }
-
-        return ans;
+        int n=arr.size();
+        int maxSum=0;
+        int i=0,j=arr.size()-1;
+        while(i<j){
+            int sum=0;
+            sum+=arr[i]+arr[n-i-1];
+            maxSum=max(maxSum,sum);
+            i++;j--;
+        }
+        return maxSum;
     }
 };
