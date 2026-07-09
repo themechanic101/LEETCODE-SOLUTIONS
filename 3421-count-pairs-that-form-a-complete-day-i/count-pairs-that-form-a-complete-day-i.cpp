92b6cf7297929aa62ac3ee8c868d4b520535b0ac
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-07-09 17:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (50.44%) - LeetSync

---
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        int n=hours.size();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((hours[i]+hours[j])%24==0) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
