c6dd49bb80a952593479cc22528b52a00d42b04e
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-07-22 19:35:43 +0530

    Time: 21 ms (8.09%) | Memory: 13.9 MB (98.09%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -7,28 +7,27 @@ public:
 
                for(int i=0; i < n; i++) {
             
-            // To store the smallest value of subarray
+            
             int smallest = nums[i];
             
-            // To store the largest value of subarray
+        
             int largest = nums[i];
             
-            /* Nested loop to get all 
-            subarrays starting from index i */
+        
             for(int j=i; j < n; j++) {
                 
-                // Update the smallest value
+            
                 smallest = min(smallest, nums[j]);
                 
-                // Update the largest value
+            
                 largest = max(largest, nums[j]);
                 
-                // Update the sum
+            
                 sum += (largest - smallest);
             }
         }
         
-        // Return the computed sum
+    
         return sum;
 
     }
