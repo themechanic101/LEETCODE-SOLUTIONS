32520ad56a6674e7b03a8d24dc589d7bc0ab3416
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-07-26 03:16:14 +0530

    Time: 63 ms (19.39%) | Memory: 21.8 MB (14.21%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -8,7 +8,7 @@ public:
        
         for (int i = 0; i < n; ++i) {
             
-            for (int j = 1; j <= nums[i] && i + j < n; ++j) {
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
             
                 dp[i + j] = min(dp[i + j], dp[i] + 1);
             }
