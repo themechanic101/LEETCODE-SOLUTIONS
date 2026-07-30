14feeeafe9de8d5599b65151d5407640ed8db4c2
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-07-30 18:37:05 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (81.70%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        stack<string>s;
-        for(int i=0;i<logs.size();i++){
-            if(logs[i]=="../"){
-              if(!s.empty()) s.pop();
+        int depth=0;
+        for(const string& log:logs){
+            if(log=="../"){
+                if(depth>0) depth--;
             }
-            else if(logs[i]=="./") continue;
-            else s.push(logs[i]);
+            else if(log=="./") continue;
+            else{
+                depth++;
+            }
         }
-        return s.size();
+        return depth;
     }
 };
