dc652795f9b0c88babbd69017209c62c980e8a2b
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-08-02 00:19:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.21%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
+    bool stoneGame(vector<int>& p) {
+        int n = p.size();
+        int a=0;
+        int l=0,r=n-1;
 
-        //mkmknk
-        return true;
+        int sum=accumulate(p.begin(),p.end(),0); 
+        while (l <= r) {
+            a+=max(p[l],p[r]);
+            l++;
+            r--;
+        }
+
+        return a>(sum-a);
     }
 };
