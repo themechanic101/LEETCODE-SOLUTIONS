76d4ce1677ccaee53e402400d88367b998b4416d
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-09-11 02:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (44.06%) - LeetSync

--- 4411-count-rotations-with-exactly-k-equal-adjacent-pairs/count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
+++ 4411-count-rotations-with-exactly-k-equal-adjacent-pairs/count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
@@ -3,20 +3,24 @@ public:
     int countRotations(string s, int k) {
         string s1=s+s;
         int n=s.size();
-        int ans=0,i=0;
+      int score=0;
 
-        while(i+n<s1.size()){
-            int score=0;
-            for(int j=0;j<n-1;j++){
-                if(s1[i+j]==s1[i+j+1]){
-                     score++;
-                }
-            }
-
-            if(score==k)ans++;
-            i++;
+      for(int j=0;j<n-1;j++){
+        if(s1[j]==s1[j+1]){
+            score++;
         }
+      }
 
-        return ans;
+      int ans=0;
+      if(score==k)ans++;
+
+      for(int i=0;i<n-1;i++){
+        if(s1[i]==s1[i+1])score--;
+
+        if(s1[i+n-1]==s1[i+n])score++;
+
+        if(score==k)ans++;
+      }
+      return ans;
     }
 };
