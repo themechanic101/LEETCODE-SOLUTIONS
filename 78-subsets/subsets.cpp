3cf980f6508f376ab94d3c1dfa93669ca4b5fcef
Utkarsh Maddhesiya <168337418+themechanic101@users.noreply.github.com>
2026-09-20 02:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (38.17%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-
+//total number of subsets is 2^n
         int subsets = 1 << n;
 
     
@@ -11,7 +11,7 @@ public:
         for (int num = 0; num < subsets; num++) {
         
             vector<int> subset;
-
+//to check if ith bit set then include
             for (int i = 0; i < n; i++) {
     
                 if (num & (1 << i)) {
